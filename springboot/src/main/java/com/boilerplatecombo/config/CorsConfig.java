77e56df97ff015893b5e31515d6a637a109bd089
EvilColdhand <marco.raditya88@gmail.com>
2025-05-18 08:40:24 +0800

    feat(springboot): Allow all CORS

---
+++ springboot/src/main/java/com/boilerplatecombo/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.boilerplatecombo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all paths
+                        .allowedOrigins("*") // Allow all origins
+                        .allowedMethods("*") // Allow all HTTP methods
+                        .allowedHeaders("*"); // Allow all headers
+            }
+        };
+    }
+}
